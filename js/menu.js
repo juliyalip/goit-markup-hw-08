f597d53bb139616029304d37468d715891aedc61
Juliya Lipatova <575@ukr.net>
2021-07-07 19:17:54 +0300

    правка - модальное окно

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,10 @@
-(() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
+
+const butnMenu = document.querySelector('.menu-button');
+const mobileMenuRef = document.querySelector("[data-menu]");
   
-    menuBtnRef.addEventListener("click", () => {
-      const expanded =
-        menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+  butnMenu.addEventListener('click', onClikcBtn)
   
-      menuBtnRef.classList.toggle("is-open");
-      menuBtnRef.setAttribute("aria-expanded", !expanded);
-  
-      mobileMenuRef.classList.toggle("is-open");
-    });
-  })();
+function onClikcBtn() {
+  butnMenu.classList.toggle('is-open')
+  mobileMenuRef.classList.toggle('is-open')
+}
