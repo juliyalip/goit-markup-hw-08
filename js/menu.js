c66814f7817c9ed061299bc9ec469501c5a57577
Juliya Lipatova <575@ukr.net>
2024-09-27 09:38:20 +0200

    minor fix

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,10 @@
 
-const butnMenu = document.querySelector('.menu-button');
+const buttonMenu = document.querySelector('.menu-button');
 const mobileMenuRef = document.querySelector("[data-menu]");
   
-  butnMenu.addEventListener('click', onClikcBtn)
+buttonMenu.addEventListener('click', onClikcBtn);
   
 function onClikcBtn() {
-  butnMenu.classList.toggle('is-open')
+  buttonMenu.classList.toggle('is-open')
   mobileMenuRef.classList.toggle('is-open')
-}
+  }
